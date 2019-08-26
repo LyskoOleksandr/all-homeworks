af14a7f25171c89ff9bfe3b27693c9ad9bba5ac8
Hater <mymac@MacBook-Pro-admin.local>
2019-08-26 18:07:12 +0300

    full

---
+++ hw1 — копия/script.js
@@ -0,0 +1,33 @@
+// S=(a*h)/2;
+
+let a = prompt('Вкажіть сторони трикутника');
+let h = prompt('Вкажіть висоту трикутника');
+let s = (a*h)/2;
+console.log('Площа трикутника ' + s);
+
+console.log('____________');
+
+const firstName = 'Oleksandr';
+console.log('First Name - ' + firstName);
+
+const lastName = 'Lysko';
+console.log('Last Name - ' + lastName);
+
+const dateOfBirth = '22.07.1998';
+console.log('Date Of Birth - ' + dateOfBirth);
+
+let age = 21;
+console.log('Age - ' + age);
+
+let height = 183;
+console.log('Height - ' + height);
+
+let weight = 58;
+console.log('Weight - ' + weight);
+
+let favouriteColor = 'Red';
+console.log('Favourite Color - ' + favouriteColor);
+
+let favouriteFilm = 'The Silence Of The Lambs';
+console.log('Favourite Film - ' + favouriteFilm);
+
